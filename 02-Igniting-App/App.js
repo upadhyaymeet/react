00cd2327e08edfbf16f12a5b7cd43765cf2d4de2
Meet <codewithmeet25@gmail.com>
2024-05-13 16:08:24 +0530

    parcel and react react dom initialize

--- 02-Igniting-App/App.js
+++ 02-Igniting-App/App.js
@@ -1,8 +1,27 @@
+import React from "react"
+import ReactDOM from "react-dom"
 
 const parent = React.createElement(
     "div",
     {id:"parent"},
-    "Hey this is div"
+    [
+        React.createElement(
+            "div",
+            {id:"child"},
+            [
+                React.createElement("h1",{},"Hey First Heading"),
+                React.createElement("h2",{},"Hey Second Heading")
+            ]
+        ),
+        React.createElement(
+            "div",
+            {id:"child2"},
+            [
+                React.createElement("h1", {}, "Hey First Heading"),
+                React.createElement("h2", {}, "Hey Second Heading")
+            ]
+        )
+    ]
 )
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
